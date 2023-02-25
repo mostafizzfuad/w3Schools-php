a70df74be2d8290464b76fd88248da19e3ebf374
mostafizz fuad <mostafizzfuad@gmail.com>
2023-02-26 02:39:44 +0600

    update code

---
+++ 02_syntax.php
@@ -0,0 +1,18 @@
+<?php
+
+// In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive.
+
+echo "Hello World ! \n";
+ECHO "Hello World ! \n";
+EcHo "Hello World ! \n";
+
+echo PHP_EOL;
+
+
+// Note: However; all variable names are case-sensitive!
+
+$color = "red";
+
+echo "My car is " . $color; // working
+// echo "My house is " . $COLOR; // not working
+// echo "My boat is " . $coLOR; // not working
---
+++ 03_comment.php
@@ -0,0 +1,20 @@
+<?php
+
+// single line comment
+# single line comment
+
+
+
+
+/* 
+
+Name : Mostafizur Rahman Fuad
+Age : 25
+Address : Mirpur , Dhaka , Bangladesh
+
+*/
+
+
+
+$x = 5 /* + 15 */ + 5;
+echo $x;
