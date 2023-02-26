0e76d7a21ae5afd0ab7f2d1efa3960577e8e96c0
mostafizz fuad <mostafizzfuad@gmail.com>
2023-02-27 00:32:34 +0600

    update code

---
+++ 08_string.php
@@ -0,0 +1,23 @@
+<?php
+
+echo strlen("Mostafizur Rahman Fuad");  // Return the Length of a String
+echo PHP_EOL;
+
+echo str_word_count("Mostafizur Rahman Fuad");  // Count Words in a String
+echo PHP_EOL;
+
+echo strrev("Mostafizur Rahman Fuad"); // Reverse a String
+echo PHP_EOL;
+
+echo strpos("Mostafizur Rahman Fuad", "Rahman"); // searches for a specific text within a string
+// note : Tip: The first character position in a string is 0 (not 1).
+
+echo PHP_EOL;
+
+echo str_replace("world", "Dolly", "Hello world!");
+echo PHP_EOL;
+echo str_replace("Fuad", "Siam", "My name is Fuad"); // replace Fuad by Siam
+
+
+
+// https://www.w3schools.com/php/php_ref_string.asp
