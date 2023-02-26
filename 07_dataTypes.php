044254a7f5a3379cde3fe8a23f7c9f7349a37ccc
mostafizz fuad <mostafizzfuad@gmail.com>
2023-02-26 21:46:15 +0600

    update code

---
+++ 07_dataTypes.php
@@ -0,0 +1,62 @@
+<?php
+
+// string 
+$name = "Mostafizur Rahman Fuad";
+echo $name;
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+
+// integer
+$x = 2023;
+var_dump($x);
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+// float
+$y = 27.563;
+var_dump($y);
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+
+// boolean
+$x = true;
+var_dump($x);
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+
+// NULL
+$x = NULL;
+var_dump($x);
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+
+
+// array
+$x = array(1,2,3);
+$name1 = array("Fuad" , "Musfiqur" , "Junnun");
+$name2 = ['Fuad' , 'Musfiqur' , 'Junnun'];
+
+var_dump($x);
+var_dump($name1);
+var_dump($name2);
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+
+
+/// object : Classes and objects are the two main aspects of object-oriented programming.
+
+
+
+// resource : The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP. A common example of using the resource data type is a database call.
+
