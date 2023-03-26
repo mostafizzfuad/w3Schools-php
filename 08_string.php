2fd51146cf07580b387bb65c3f53a858a4736394
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-03-27 03:12:49 +0600

    update code

--- 08_string.php
+++ 08_string.php
@@ -3,20 +3,43 @@
 echo strlen("Mostafizur Rahman Fuad");  // Return the Length of a String
 echo PHP_EOL;
 
+
 echo str_word_count("Mostafizur Rahman Fuad");  // Count Words in a String
 echo PHP_EOL;
+
 
 echo strrev("Mostafizur Rahman Fuad"); // Reverse a String
 echo PHP_EOL;
 
+
 echo strpos("Mostafizur Rahman Fuad", "Rahman"); // searches for a specific text within a string
 // note : Tip: The first character position in a string is 0 (not 1).
 
-echo PHP_EOL;
 
-echo str_replace("world", "Dolly", "Hello world!");
 echo PHP_EOL;
 echo str_replace("Fuad", "Siam", "My name is Fuad"); // replace Fuad by Siam
+
+
+echo PHP_EOL;
+echo str_repeat("Fuad",7); // Repeats a string a specified number of times
+
+
+echo PHP_EOL;
+echo strcmp("Hello BD!","Hello BD!");  // returns 0, if the two strings are equal.
+
+
+echo PHP_EOL;
+echo strtolower("MOSTAFIZUR RAHMAN FUAD"); // convert lowercase
+
+
+echo PHP_EOL;
+echo strtoupper("mostafizur rahman fuad"); // convert uppercase
+
+
+echo PHP_EOL;
+echo substr("Hello Mostafizur",6); // returns a part of a string. (6 means - কত নাম্বার ইন্ডেক্স এর পর থেকে রিটার্ন করবে)
+// note : Tip: The first character position in a string is 0 (not 1).
+
 
 
 
--- 09_numbers.php
+++ 09_numbers.php
@@ -1,1 +1,83 @@
 <?php
+
+// ********** Number System Representation ********** //
+$decimal = 12;
+$octal = 015;
+$hexaDecimal = 0x2B;
+
+
+
+
+// ***** convert decimal to others ***** //
+printf("%b", $decimal); // convert binary
+echo PHP_EOL;
+printf("%x", $decimal); // convert hexadecimal
+echo PHP_EOL;
+printf("%o", $decimal); // convert octal
+
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+// ********** Check if the type of a variable is integer, float, finite/infinite, NaN :  ********** //
+// check integer
+$x = 5985;
+var_dump(is_int($x));
+$x = 59.85;
+var_dump(is_int($x));
+    
+    
+// check float
+$x = 10.365;
+var_dump(is_float($x));
+    
+    
+// Check finite or infinite
+$x = 1.9e411;
+var_dump($x);
+    
+    
+// Invalid calculation will return a NaN value (NaN stands for Not a Number.)
+$x = acos(8);
+var_dump($x);
+
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+
+// ********* Check if the variable is numeric  ******** //
+$x = 5985;
+var_dump(is_numeric($x));
+
+$x = "5985";
+var_dump(is_numeric($x));
+
+$x = "59.85" + 100;
+var_dump(is_numeric($x));
+
+$x = "Hello";
+var_dump(is_numeric($x));
+
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+
+// *******  Type Casting ********** // 
+// ********** Cast float to int ********** //
+$x = 23465.768;
+var_dump($x);
+
+$convert = (int)$x;
+var_dump($convert);
+
+echo PHP_EOL;
+
+
+// ********** Cast string to int ********** //
+$x = "358445.768";
+var_dump($x);
+
+$convert = (int)$x;
+var_dump($convert);
