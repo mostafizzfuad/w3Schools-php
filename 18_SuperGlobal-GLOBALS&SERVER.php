c48659c4e2e5002ab1aa96a4c8507470d06ac37f
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-05-05 17:32:15 +0600

    update code

--- 18_SuperGlobal-GLOBALS&SERVER.php
+++ 18_SuperGlobal-GLOBALS&SERVER.php
@@ -34,6 +34,8 @@ echo PHP_EOL;
 
 
 /********* $_SERVER *********/
+
+/*********
 echo $_SERVER['PHP_SELF']; // Returns the filename of the currently executing script
 
 echo $_SERVER['SERVER_NAME']; // Returns the name of the host server (such as www.w3schools.com)
@@ -49,6 +51,6 @@ echo $_SERVER['SCRIPT_NAME']; // Returns the path of the current script
 echo $_SERVER['SERVER_ADDR']; // Returns the IP address of the host server
 
 echo $_SERVER['REQUEST_METHOD']; // Returns the request method used to access the page (such as POST)
-
+*********/
 
 // https://www.w3schools.com/php/php_superglobals_server.asp
