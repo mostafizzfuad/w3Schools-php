43dd9662de8d18df1b3379ce930bd3f6a5652fcc
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-05-05 17:27:52 +0600

    update code

---
+++ 18_SuperGlobal-GLOBALS&SERVER.php
@@ -0,0 +1,54 @@
+<?php
+
+/********* Superglobal *********
+
+The PHP superglobal variables are:
+$GLOBALS
+$_SERVER
+$_REQUEST
+$_POST
+$_GET
+$_FILES
+$_ENV
+$_COOKIE
+$_SESSION
+
+ ********************************/
+
+
+
+/*********  $GLOBALS *********/
+$x = 75;
+$y = 25;
+ 
+function addition() {
+  $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
+ 
+addition();
+echo $z;
+
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+
+/********* $_SERVER *********/
+echo $_SERVER['PHP_SELF']; // Returns the filename of the currently executing script
+
+echo $_SERVER['SERVER_NAME']; // Returns the name of the host server (such as www.w3schools.com)
+
+echo $_SERVER['HTTP_HOST']; // Returns the Host header from the current request
+
+echo $_SERVER['HTTP_REFERER']; // Returns the complete URL of the current page
+
+echo $_SERVER['HTTP_USER_AGENT']; // Returns the browser's name
+
+echo $_SERVER['SCRIPT_NAME']; // Returns the path of the current script
+
+echo $_SERVER['SERVER_ADDR']; // Returns the IP address of the host server
+
+echo $_SERVER['REQUEST_METHOD']; // Returns the request method used to access the page (such as POST)
+
+
+// https://www.w3schools.com/php/php_superglobals_server.asp
---
+++ 19_SuperGlobal-ReqPostGet.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuperGlobal - REQUEST , POST & GET</title> 
+</head>
+<body>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">  
+    <!-- $SERVER['PHP_SELF'] : same page এর url কে ধরার জন্য ইউজ করা হয়েছে । অর্থাৎ, action একই পেজের মধ্যে ঘটবে -->
+        <label> Name 
+            <input type="text" name="fname"> 
+            <input type="submit" value="Submit">
+        </label>
+    </form>
+
+    <?php
+    // $_POST is also widely used to pass variables.
+    // $_GET can also collect data sent in the URL.
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+            $name = $_REQUEST['fname']; // collect value of input field
+            if (empty($name)) {
+                echo "Name is empty";
+            } else {
+                echo $name;
+            }
+        }
+    ?>
+</body>
+</html>
