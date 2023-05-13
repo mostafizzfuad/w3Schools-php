63053f48b90a316dd534c1179b364e0240fddfc7
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-05-13 22:50:34 +0600

    update code

---
+++ 21_formHandling.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Handling</title>
+</head>
+
+<body>
+
+    <form action="welcome.php" method="post">
+        Name: <input type="text" name="name" placeholder="Write Your Name" required><br><br>
+        Email: <input type="email" name="email" placeholder="Write Your Email" required><br><br>
+        Phone: <input type="tel" name="phone" placeholder="Write Your Phone number" required><br><br>
+        <input type="submit">
+    </form>
+
+    
+</body>
+
+</html>
---
+++ 21_welcome.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+
+    Welcome <?php echo $_POST["name"]; ?><br>
+    Your email address is: <?php echo $_POST["email"]; ?><br>
+    Your phone number is: <?php echo $_POST["phone"] ?>
+
+</body>
+
+</html>
